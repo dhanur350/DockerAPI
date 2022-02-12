12cccc910a0f79949e100cd34230e8b99b8602a2
dhanur350 <dhanur297@gmail.com>
2022-02-12 17:58:36 +0530

    DockerDay1

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DockerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Value1", "Value2" };
+        }
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DockerAPI
-To Deploy .NET Core 5.0 with Docker
+To Deploy .NET Core 5.0 with Docker in Linux 
+I used Ubuntu 20.04 LTS with .NET Core 5.0 and Docker installed in this OS
+Rest Instructions are in the notes
